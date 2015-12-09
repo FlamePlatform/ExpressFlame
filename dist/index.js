4d9c1694bd4571f153f041d0f265ad0c108b04c5
Shavauhn Gabay <shavauhng@i3inc.local>
2015-12-09 10:58:58 -0500

    updated build

---
+++ dist/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.ExpressFlame = ExpressFlame;
+
+var _promise = require("babel-runtime/core-js/promise");
+
+var _promise2 = _interopRequireDefault(_promise);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function ExpressFlame(func) {
+  return (function (func) {
+    return function express_middleware(req, res) {
+      return new _promise2.default(function (resolve, reject) {
+        func.call(null, req, res, function (err) {
+          if (err) return reject(err);
+          resolve();
+        });
+      });
+    };
+  })(func);
+}
